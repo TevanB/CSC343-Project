6741e3ed045f392cd54c679dee64cf9087b9e2cd
Tevan B <tevan.bhatia@mail.utoronto.ca>
2021-11-24 18:28:21 -0500

    row change added to cleaner file

--- data/cleaner.py
+++ data/cleaner.py
@@ -13,8 +13,9 @@ country_df = None
 individual_df = None
 happiness_df = None 
 personality_df = bfd_df
-personality_df.rename(columns={"case_id": "pID", "openness_score":"openness", "conscientiousness_score": "conscientious", "neuroticism_score":"neuroticism"}, inplace=True)
+personality_df.rename(columns={"case_id": "pID","extraversion_score": "extraversion", "openness_score":"openness", "conscientiousness_score": "conscientious", "neuroticism_score":"neuroticism"}, inplace=True)
 personality_df.drop(["country", "age", "sex"], inplace=True, axis=1)
+personality_df.reset_index(drop=True, inplace=True)
 personality_df.to_csv('phase1/personality.csv')
 print(personality_df.head())
 #print(whr_df.head())
