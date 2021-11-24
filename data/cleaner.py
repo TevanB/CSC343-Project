cba68fc1b7ee8718ca5553855b8ad45a7858d68e
Tevan B <tevan.bhatia@mail.utoronto.ca>
2021-11-24 18:56:38 -0500

    restructuring and cleaner file update

--- data/cleaner.py
+++ data/cleaner.py
@@ -1,21 +1,36 @@
 import pandas as pd 
 import numpy as np 
 
-bfd_src = 'phase1/big_five_data.csv'
-whr_src = 'phase1/world_health_report.csv'
+bfd_src = 'phase1_sample_data/big_five_data.csv'
+whr_src = 'phase1_sample_data/world_happiness_report.csv'
+cotw_src = 'phase1_sample_data/country_info_data.csv'
 
 bfd_df = pd.read_csv(bfd_src)
 whr_df = pd.read_csv(whr_src)
+cotw_df = pd.read_csv(cotw_src)
 
 
+country_df = cotw_df
+country_df.rename(columns={"Country":"country_name", "GDP ($ per capita)": "gdp", "Birthrate": "birth_rate"}, inplace=True)
+country_df.drop(["Region", "Population", "Area (sq. mi.)", "Pop. Density (per sq. mi.)", "Coastline (coast/area ratio)", "Net migration", "Infant mortality (per 1000 births)", "Literacy (%)", "Phones (per 1000)", "Arable (%)", "Crops (%)", "Other (%)", "Climate", "Deathrate", "Agriculture", "Industry", "Service"], inplace=True, axis=1)
+country_df.reset_index(drop=True, inplace=True)
+country_df.to_csv('phase1/country.csv', index=False, header=False)
 
-country_df = None
+
+happiness_df = whr_df 
+happiness_df.rename(columns={"Country":"country_name", "Happiness Score":"happiness_score", "Economy (GDP per Capita)":"gdp", "Family":"family"}, inplace=True)
+happiness_df.drop(["Region","Happiness Rank","Standard Error","Health (Life Expectancy)","Freedom","Trust (Government Corruption)","Generosity","Dystopia Residual"] , inplace=True, axis=1)
+happiness_df.reset_index(drop=True, inplace=True)
+happiness_df.to_csv('phase1/happiness.csv', index=False, header=False)
+
+
 individual_df = None
-happiness_df = None 
+
+
+
 personality_df = bfd_df
 personality_df.rename(columns={"case_id": "pID","extraversion_score": "extraversion", "openness_score":"openness", "conscientiousness_score": "conscientious", "neuroticism_score":"neuroticism"}, inplace=True)
 personality_df.drop(["country", "age", "sex"], inplace=True, axis=1)
 personality_df.reset_index(drop=True, inplace=True)
 personality_df.to_csv('phase1/personality.csv', index=False, header=False)
-print(personality_df.head())
 #print(whr_df.head())
