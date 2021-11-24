ac39436aa49f63038059aed472bc69f011b23248
Bhatia Tevan Taharally K <bhatiate@teach.cs.toronto.edu>
2021-11-24 18:18:17 -0500

    starter cleaning file

---
+++ data/cleaner.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import numpy as np 
+
+bfd_src = 'phase1/big_five_data.csv'
+whr_src = 'phase1/world_health_report.csv'
+
+bfd_df = pd.read_csv(bfd_src)
+whr_df = pd.read_csv(whr_src)
+
+
+
+country_df = None
+individual_df = None
+happiness_df = None 
+personality_df = bfd_df
+personality_df.rename(columns={"case_id": "pID", "openness_score":"openness", "conscientiousness_score": "conscientious", "neuroticism_score":"neuroticism"}, inplace=True)
+personality_df.drop(["country", "age", "sex"], inplace=True, axis=1)
+personality_df.to_csv('phase1/personality.csv')
+print(personality_df.head())
+#print(whr_df.head())
